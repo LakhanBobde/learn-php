b056e3cc86e1638959a02247c44b68aaccc1cd1a
Lakhan Bobde <lakhanbobde96@gmail.com>
2018-11-29 12:39:05 +0530

    index.php added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <form action="./post.php" method="post">
+<b>Username</b><br/>
 <input type="text" name="uname">
-
+<b>Password</b><br/>
 <input type="password" name="password">
-
 <input type="submit" value="Login Now">
 </form>
