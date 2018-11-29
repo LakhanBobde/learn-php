c72d189ecd6551fc219739e6a19c68dac96638f8
Lakhan Bobde <lakhanbobde96@gmail.com>
2018-11-29 22:05:04 +0530

    push.php added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <form action="./post.php" method="post">
 <b>Username</b><br/>
-<input type="text" name="uname">
+<input type="text" name="user"><br/>
 <b>Password</b><br/>
-<input type="password" name="password">
+<input type="password" name="pass"><br/>
 <input type="submit" value="Login Now">
 </form>
--- post.php
+++ post.php
@@ -1,4 +1,17 @@
 <?php
-header('Location: ./dashboard.php');
+//header('Location: ./dashboard.php');
 // var_dump($_POST);
+$user = $_POST['user'];
+$pass = $_POST['pass'];
 
+echo"User name is " .$user ;
+echo"<br/>User password " .$pass;
+
+if($user == "lakhan" && $pass == "qwerty")
+{
+    echo"<br>login successful";
+}
+else
+{
+     echo"<br>login failed";
+}
