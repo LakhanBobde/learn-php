9e44a9218c8fb701fbe6c2043eb3f2ce770478f0
Lakhan Bobde <lakhanbobde96@gmail.com>
2018-11-29 11:44:42 +0530

    push.php added

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<?php
-echo 'welcome';
+<form action="./post.php" method="post">
+<input type="text" name="uname">
+
+<input type="password" name="password">
+
+<input type="submit" value="Login Now">
+</form>
---
+++ post.php
@@ -0,0 +1,4 @@
+<?php
+header('Location: ./dashboard.php');
+// var_dump($_POST);
+
