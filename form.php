83b43d6b295a313ebc58f5971489db650327c583
Lakhan Bobde <lakhanbobde96@gmail.com>
2018-12-12 16:39:57 +0530

    modifies form

--- form.php
+++ form.php
@@ -1,34 +1,4 @@
-<?php 
-if(isset($_POST['btn']))
-{
-    $name = array("Maria","Jain","Tom");
-    $minimum = 5;
-    $maximum = 10;
-    $username = $_POST['username'];
-    $password = $_POST['password'];
- 
-    //echo "Welcome " . $username;
-    //echo "<br>Your password " . $password;
-
-    if(strlen($username)< $minimum)
-    {
-        echo "<br>Username is longer than five ";
-    }
-    if(strlen($username)> $maximum)
-    {
-        echo "<br>Username is longer than ten ";
-    }
-
-    if(in_array($username,$name)){
-        echo "Sorry you are not allowed!";
-    } else{
-        echo "Welcome";
-    }
-
-
-}
-?>
-<form action="./form.php" method="post">
+<form action="./form_process.php" method="post">
 <input type ="text" name="username" placeholder="Enter the Name"><br>
 <input type ="password" name="password" placeholder="Enter the Password">
 <br>
